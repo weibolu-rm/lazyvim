7a0af4c5aabecf40cd71ae8b9c3bad799ab24d88
Robert Michaud <robert.cn.michaud@hotmail.com>
2024-05-19 21:25:14 -0400

    updated to nvim 0.10.0

--- lua/plugins/luasnip.lua
+++
@@ -1,7 +0,0 @@
--- Disable default tab behavior to enable super-tab in cmp
-return {
-  "L3MON4D3/LuaSnip",
-  keys = function()
-    return {}
-  end,
-}
--- lua/plugins/smart-yank.lua
+++ lua/plugins/smart-yank.lua
@@ -1,1 +1,3 @@
-return { "ibhagwan/smartyank.nvim" }
+return { "ibhagwan/smartyank.nvim", opts = {
+  osc52 = { ssh_only = false },
+} }
